26967e60c7849e27f598ad0679cea3f196967b91
trolleybusartem <112125354+trolleybusartem@users.noreply.github.com>
2022-11-27 18:09:37 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import kivy
 from playsound import playsound
 kivy.require("1.9.1")
+import kivy
+from playsound import playsound
+kivy.require("1.9.1")
 from kivy.app import App
 from kivy.uix.button import Button
 class Holodilnik(App):
     def build(self):
-        btn = Button(text="Нажми чтобы поговорить с Гелей !",
-                     font_size="20sp",
-                     background_color=(1, 1, 1, 1),
+        btn = Button(text="Нажми чтобы услышать холодильник!",
+                     font_size="15sp",
+                     background_color=(1, 76, 12, 1),
                      color=(1, 1, 1, 1),
                      size=(8, 8),
                      size_hint=(.4, .4),
-                     pos=(300, 250))
+                     pos=(250, 150))
         btn.bind(on_press=self.callback)
         return btn
     def callback(self, event):
